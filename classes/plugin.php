50307aac295f9d5d056807f39d86c281ae20b732
Mark Jaquith <mark@jaquith.me>
2013-12-07 02:07:09 -0500

    Add an admin menu stub

--- classes/plugin.php
+++ classes/plugin.php
@@ -3,9 +3,11 @@ defined( 'WPINC' ) or die;
 
 class Admin_Color_Schemer_Plugin {
 	private static $instance;
+	private $base;
 
 	protected function __construct() {
 		self::$instance = $this;
+		$this->base = dirname( dirname( __FILE__ ) );
 		add_action( 'init', array( $this, 'init' ) );
 	}
 
@@ -17,7 +19,18 @@ class Admin_Color_Schemer_Plugin {
 	}
 
 	public function init() {
+		// Initialize translations
 		load_plugin_textdomain( 'admin-color-schemer', false, basename( dirname( dirname( __FILE__ ) ) ) . '/languages' );
-		// More hooks here
+
+		// Hooks
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+	}
+
+	public function admin_menu() {
+		add_management_page( 'Admin Color Schemer', 'Admin Colors', 'manage_options', 'admin-color-schemer', array( $this, 'admin_page' ) );
+	}
+
+	public function admin_page() {
+		include( $this->base . '/templates/admin-page.php' );
 	}
 }
---
+++ templates/admin-page.php
@@ -0,0 +1,9 @@
+<?php
+defined( 'WPINC' ) or die;
+?>
+<div class="wrap">
+	<h2><?php echo esc_html( $GLOBALS['title'] ); ?></h2>
+
+	<!--stub-->
+
+</div>
