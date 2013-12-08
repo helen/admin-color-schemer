c3812d8a3b5922143ad86f23f969b1a38a457163
Mark Jaquith <mark@jaquith.me>
2013-12-07 23:19:32 -0500

    Start filling out the Admin_Color_Schemer_Scheme class more.

--- classes/color-scheme.php
+++ classes/color-scheme.php
@@ -2,17 +2,21 @@
 defined( 'WPINC' ) or die;
 
 class Admin_Color_Schemer_Scheme {
+	protected $id;
+	protected $name;
+	protected $accessors = array( 'base', 'highlight', 'notification', 'button', 'id' );
+
+	// Colors
 	protected $base;
 	protected $highlight;
 	protected $notification;
 	protected $button;
-	protected $accessors = array( 'base', 'highlight', 'notification', 'button' );
 
-	public function __construct( $colors = NULL ) {
-		if ( is_array( $colors ) ) {
+	public function __construct( $attr = NULL ) {
+		if ( is_array( $attr ) ) {
 			foreach ( $this->accessors as $thing ) {
-				if ( isset( $colors[$thing] ) ) {
-					$this->{$thing} = $colors[$thing];
+				if ( isset( $attr[$thing] ) ) {
+					$this->{$thing} = $attr[$thing];
 				}
 			}
 		}
@@ -20,13 +24,28 @@ class Admin_Color_Schemer_Scheme {
 
 	public function __get( $key ) {
 		if ( in_array( $key, $this->accessors ) ) {
-			return $this->{$key};
+			return $this->sanitize( $this->{$key}, $key, 'out' );
 		}
 	}
 
 	public function __set( $key, $value ) {
 		if ( in_array( $key, $this->accessors ) ) {
-			$this->{$key} = $value;
+			$this->{$key} = $this->sanitize( $value, $key, 'in' );
 		}
+	}
+
+	private function sanitize( $value, $key, $direction ) {
+		switch ( $key ) {
+			case 'id':
+				$value = absint( $value );
+				break;
+			case 'base':
+			case 'highlight':
+			case 'notification':
+			case 'button':
+				$value = preg_replace( '@[^a-f0-9]@', '', strtolower( substr( trim( $value ), 0, 6 ) ) );
+				break;
+		}
+		return $value;
 	}
 }
